07383d6b6dd68a76c335996b6943f6724ad18e5b
Madhur-Jain-08 <143706883+Madhur-Jain-08@users.noreply.github.com>
2023-08-31 16:52:17 +0530

    Add files via upload

---
+++ Project-3/index.js
@@ -0,0 +1,49 @@
+console.log("This is my index js file");
+
+// Initialize the news api parameters
+let source = 'country=in';
+let apiKey = '4c754f3d23974b5ba54375732d07f916'
+
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?${source}&apiKey=${apiKey}`, true);
+
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status == 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        // console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index);
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                        aria-expanded="true" aria-controls="collapse${index}">
+                                        <b>Breaking News ${index+1}:</b>
+                                        ${element["title"]}
+                                    </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}"
+                                data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank">Read more here</a>
+                                </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some error occured");
+    }
+}
+
+xhr.send();
