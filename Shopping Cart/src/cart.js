ea47749fe32fd6b50d6374ff172b82557a65d553
MADHUR <madhurjain677@gmail.com>
2023-10-19 15:23:14 +0530

    Cart Changes

--- Shopping Cart/src/cart.js
+++ Shopping Cart/src/cart.js
@@ -18,7 +18,7 @@ let generateCartItems = () => {
         let search = shopItemsData.find((y) => y.id === id) || [];
         return `
       <div class="cart-item">
-        <img width="100" src=${search.img} alt="" />
+        <img width="100" height="150" src=${search.img} alt="" />
         <div class="details">
 
           <div class="title-price-x">
