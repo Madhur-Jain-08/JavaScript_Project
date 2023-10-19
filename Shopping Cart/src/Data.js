8aa605d6819cb9cc3415920341bcf7ea02cb6cbe
MADHUR <madhurjain677@gmail.com>
2023-10-19 14:33:05 +0530

    SHopping Cart Added

---
+++ Shopping Cart/src/Data.js
@@ -0,0 +1,62 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+
+
+
+    
+    {
+      id: "thysdfdvcv",
+      name: "Track Suit",
+      price: 900,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/track-suit.jpg",
+    },
+    {
+      id: "thyfsfdgcv",
+      name: "Sports Suit",
+      price: 1300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/sports.jpg",
+    },
+    {
+      id: "thjhgsbcv",
+      name: "Kids",
+      price: 1900,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/kids.jpg",
+    },
+    {
+      id: "thyshgdhbcv",
+      name: "Infants",
+      price: 1500,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/infants.jpg",
+    },
+  ];
